3db725d594f4a1afae3489a520814c736f2e99ee
Mohammed Mazin <muhammedmazin38@gmail.com>
2025-03-23 10:32:14 +0530

    Implement compression middleware to reduce response size for improved performance

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
+import compression from 'compression'
 import usersRoutes from './routes/usersRoutes.js'
 
 const app = express()
@@ -9,6 +10,9 @@ const app = express()
 app.use(cors())
 // security
 app.use(helmet())
+app.use(compression({
+    threshold: 1024  // Only compress responses larger than 1KB
+}))
 dotenv.config()
 app.use(express.json())
 app.use('/api/v1/users', usersRoutes)
